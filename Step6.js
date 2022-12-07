b0441a324e692c6f0d5d6f1d79d0877fdafcb192
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-07 11:07:07 +0200

    Step6 done

---
+++ Step6.js
@@ -0,0 +1,6 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+const division = num1 / num2;
+
+alert (`the remainder of ${num1} divided by ${num2} is ${division}`);
