019f0186f773c4ad9008920c9189b3c664b9677b
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-07 10:26:51 +0200

    Step5 done

---
+++ Step5.js
@@ -0,0 +1,6 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+const multiplication = num1 * num2;
+
+alert (`The multiplication of ${num1} and ${num2} is ${multiplication}`);
