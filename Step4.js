e2aa26da67d7c88861ce9c811b37a42bfbdeff19
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-07 10:17:42 +0200

    Step4 done

---
+++ Step4.js
@@ -0,0 +1,4 @@
+var name =  window.prompt ("What is your name?");
+var surname = window.prompt ("What is your surname?");
+var city = window.prompt ("Which city you are from?");
+alert ("Name: " + name + "\nSurname: "+ surname + "\nCity: "+ city);
